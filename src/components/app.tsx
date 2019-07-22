b753583a0765b715abfa285f56814a34b46ea48e
sonaksh <sonaksh@swiftace.ai>
2019-07-23 00:57:12 +0530

    vulnerability fixes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,7 +3,7 @@ author='Sonaksh'
 github.com/sonakshs*/
 
 import { h, Component } from "preact";
-import 'preact-material-components/LayoutGrid/style.css';
+import "preact-material-components/LayoutGrid/style.css";
 import Header from "./header";
 import Dashboard from "./dashboard";
 import Login from "./login";
@@ -12,7 +12,7 @@ import style from "./style.css";
 interface State {
   userId: number;
   availableUsers: object[];
-  loggedIn: boolean
+  loggedIn: boolean;
 }
 
 export default class App extends Component<any, any> {
@@ -24,37 +24,46 @@ export default class App extends Component<any, any> {
 
   componentDidMount() {
     var availableUsers = this.state.availableUsers;
-    console.log(availableUsers,'available')
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => json.map(user => availableUsers.push({ 'id': user.id, 'email': user.email })))
-    .then(() => this.setState({availableUsers}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json =>
+        json.map(user =>
+          availableUsers.push({ id: user.id, email: user.email })
+        )
+      )
+      .then(() =>
+        this.setState({ availableUsers }, () =>
+          console.log(this.state.availableUsers)
+        )
+      );
   }
 
-  login = (userId) => {
-    this.setState({ userId, loggedIn: true })
-  }
+  login = userId => {
+    this.setState({ userId, loggedIn: true });
+  };
 
   logout = () => {
     this.setState({ userId: null, loggedIn: false });
-  }
+  };
 
-  render({},{ userId, loggedIn }) {
+  render({}, { userId, loggedIn }) {
     return (
       <div id="app" class={style.mainGrid}>
-          {loggedIn?
+        {loggedIn ? (
           <div>
-            <Header logout={this.logout}/>
+            <Header logout={this.logout} />
           </div>
-          :
-          null}
-          <div>
-          {loggedIn?
-            <Dashboard userId={userId}/>
-            :
-            <Login availableUsers={this.state.availableUsers} setUserId={this.login}/>
-          }
-          </div>
+        ) : null}
+        <div>
+          {loggedIn ? (
+            <Dashboard userId={userId} />
+          ) : (
+            <Login
+              availableUsers={this.state.availableUsers}
+              setUserId={this.login}
+            />
+          )}
+        </div>
       </div>
     );
   }
